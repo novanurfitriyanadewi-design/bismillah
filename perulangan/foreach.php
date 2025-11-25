b8f125b0023a64c6680f101a14785e69ab354427
NOVA <novanurfitriyanadewi@saintek.unipdu.ac.id>
2025-11-25 08:19:12 +0700

    Add files via upload
    tugas

---
+++ perulangan/foreach.php
@@ -0,0 +1,15 @@
+<?php
+$books = [
+    "Panduan belajar PHP untuk pemula",
+    "Membangun aplikasi web dengan PHP",
+    "Tutorial PHP dan MYSQL",
+    "Membuat chat BOT dengan PHP"
+
+];
+
+echo "<H5> Judul buku PHP:</h5>";
+echo "<ul>/n";
+foreach($books as $buku){
+    echo "<li>buku</li>";
+}
+echo "</ul>/n";
